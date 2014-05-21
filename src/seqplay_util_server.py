0eafd6b432a05d7f7f809b21cc66579a2b89e238
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2014-05-21 20:01:07 +0900

    register service setTargetPoseRelative

--- src/seqplay_util_server.py
+++ src/seqplay_util_server.py
@@ -16,6 +16,7 @@ from tf.transformations import euler_from_matrix
 from hrpsys_ros_bridge.srv import *
 from nextage_ros_bridge import nextage_client
 
+
 class NextageSeqPlayUtil(object):
     def __init__(self):
         self.init_pose = nextage_client.NextageClient.InitialPose
@@ -39,6 +40,9 @@ class NextageSeqPlayUtil(object):
         self.wait_interpolation = rospy.ServiceProxy(
             '/SequencePlayerServiceROSBridge/waitInterpolation',
             OpenHRP_SequencePlayerService_waitInterpolation)
+        self.wait_interpolation_of_group = rospy.ServiceProxy(
+            '/SequencePlayerServiceROSBridge/waitInterpolationOfGroup',
+            OpenHRP_SequencePlayerService_waitInterpolationOfGroup)
         self.set_joint_angles_of_group = rospy.ServiceProxy(
             '/SequencePlayerServiceROSBridge/setJointAnglesOfGroup',
             OpenHRP_SequencePlayerService_setJointAnglesOfGroup)
@@ -61,7 +65,6 @@ class NextageSeqPlayUtil(object):
         rpy = numpy.array(euler_from_matrix([matrix[0:3], matrix[4:7], matrix[8:11]], 'sxyz'))
         pos += [delta_pos[0], delta_pos[1], delta_pos[2]]
         rpy += [delta_rpy[0], delta_rpy[1], delta_rpy[2]]
-
         return self.set_target_pose(name, list(pos), list(rpy), tm)
 
     def go_pose(self, pose, tm):
@@ -79,9 +82,14 @@ class NextageSeqPlayUtil(object):
 
 if __name__ == '__main__':
     util = NextageSeqPlayUtil()
-    print util.go_initial(2.0)
-    print util.wait_interpolation()
-    print util.set_target_pose_relative('rarm', [0, 0, 0.2], [0, 0, 0], 2.0)
-    print util.wait_interpolation()
-    print util.go_off_pose(2.0)
-    print util.wait_interpolation()
+
+    def set_target_pose_relative(request):
+        return OpenHRP_SequencePlayerService_setTargetPoseResponse(
+            util.set_target_pose_relative(request.name, request.xyz, request.rpy, request.tm))
+
+    # regsiter service /nextage_ros_seqplay_util/setTargetPoseRelative
+    rospy.init_node('nextage_ros_seqplay_util')
+    print rospy.Service('nextage_ros_seqplay_util/setTargetPoseRelative',
+                        OpenHRP_SequencePlayerService_setTargetPose,
+                        set_target_pose_relative)
+    rospy.spin()
